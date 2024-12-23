30847c40e71311b51a6009575ed05c0f9b95f32a
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-12-23 19:14:55 +0530

    feat: applicatoin loader implemented

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+
+export default function Loader() {
+  return (
+    <div className="wrapper">
+        <div className='spinner-wrapper'>
+            <div className='spinner'> </div>
+        </div>
+    </div>
+  )
+};
